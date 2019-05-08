3ad7bef387ff6c32f83e4ee8b7761b4aa71fef04
shikaids <nelson.leong@qq.com>
2019-05-08 18:53:21 +0000

    粉丝数据表

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Status;
+use Auth;
 
 class StatusesController extends Controller
 {
@@ -13,7 +14,16 @@ class StatusesController extends Controller
         $this->middleware('auth');
     }
 
-    public function store()
+    /**
+     *
+     * user的id是怎么传递到user_id的？
+     * 因为user模型的users表中id是user id，
+     * Statuses表的user_id是存储user id的，
+     * 但是怎么传递过去的？
+     * 难道是约定俗成的，users的单数就是user，所以其他表关联就是user_id，
+     * 如果这个猜想是对的，那么statuses的id关联到其他表的字段就是status_id
+     */
+    public function store(Request $request)
     {
         $this->validate($request, [
             'content' => 'required|max:140'
--- app/Models/User.php
+++ app/Models/User.php
@@ -78,9 +78,106 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    // 获取用户发布过的所有微博，并按创建时间的倒序排序。
+    /**
+     * 将当前用户发布过的所有微博从数据库中取出，并根据创建时间来倒序排序
+     * @return array 按时间倒序排序的微博列表
+     *
+     * 为什么在模型类内创建？而不是在用户控制器类创建？
+     * 因为可以被其他控制器调用，例如首页控制器。
+     * 如果是UsersController写feed()方法，那么就要以UsersController注入StaticPagesController，
+     * 但是UsersController是控制器，所以这样不合适
+     *
+     * 最后的方法就是在User模型建立要使用的数据，然后被其他控制器调用
+     */
     public function feed()
     {
         return $this->statuses()->orderBy('created_at', 'desc');
+    }
+
+
+    /**
+     * 以下代码之所以难以理解，是因为用户模型与用户模型的多对多关系，也就是user_user。
+     * 所以把建立belongsToMany关联都写在一个模型中了，而关联表则是followers。
+     *
+     * 如果以用户模型users与用户角色模型roles建立多对多关系统，那么关联表就是role_user
+     * 具体实现情况官方文档关于Eloquent关联里有说明，比较容易理解。
+     *
+     * 但是user_user的关联，必须把belongsToMany写在同一个模型中，所以如果不理解多对多的实现，
+     * 就很难理解为什么这样写。
+     *
+     * 我认为，这个教程最大败笔是把user_user的关联表命名为followers。
+     * 因为本身就是用户关注用户的，本模型的外键名是user_id, 另一个模型的外键名是follower_id，关注者id；
+     * 所以表名应该尽量区别两个模型，有助于理解模型users，与user_follower表的区别，直接用follower，
+     * 让人以为这是两个一个模型的表，而不是关联表，而且在文案写的是粉丝关系表，表述有问题，关联表就是关联表，
+     * 用“粉丝关系表”来表述，让人就是一个模型表。
+     *
+     */
+
+    /**
+     * 一个用户拥有多个粉丝。获取粉丝列表，也就是被关注的列表。
+     *
+     * 1 在 Laravel 中会默认将两个关联模型的名称进行合并，并按照字母排序，因此我们生成的关联关系表名称会是 user_user。
+     * 2 我们也可以自定义生成的名称，把关联表名改为 followers。
+     * 所以，belongsToMany的第二个参数就是自定义关联表的名称。
+     *
+     * 除了自定义合并数据表的名称，我们也可以通过传递额外参数至 belongsToMany 方法来自定义数据表里的字段名称。
+     * 1 第三个参数是定义此关联的模型在连接表里的外键名
+     * 2 第四个参数是另一个模型在连接表里的外键名
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     * 一个用户关注多个人。获取用户关注人的列表。
+     *
+     *
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * Eloquent 模型为多对多提供的一系列简便的方法。
+     *
+     * attach 方法或 sync 方法在中间表上创建一个多对多记录；
+     * 使用 detach 方法在中间表上移除一个记录；
+     * 创建和移除操作并不会影响到两个模型各自的数据，所有的数据变动都在 中间表 上进行。
+     * attach, sync, detach 这几个方法都允许传入 id 数组参数。
+     * allRelatedIds 是 Eloquent 关联关系提供的 API，用来获取关联模型的 ID 集合
+     */
+    /**
+     * sync 方法会接收两个参数，第一个参数为要进行添加的 id，
+     * 第二个参数则指明是否要移除其它不包含在关联的 id 数组中的 id，true 表示移除，false 表示不移除，
+     * 默认值为 true。
+     * 由于我们在关注一个新用户的时候，仍然要保持之前已关注用户的关注关系，因此不能对其进行移除，
+     * 所以在这里我们选用 false.
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids')
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids')
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 我们还需要一个方法用于判断当前登录的用户 A 是否关注了用户 B，
+     * 代码实现逻辑很简单，我们只需判断用户 B 是否包含在用户 A 的关注人列表上即可。
+     *
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
     }
 }
